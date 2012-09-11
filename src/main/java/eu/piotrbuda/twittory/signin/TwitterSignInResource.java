d8ee37f13e9b1ca99d26f8f08de3900fccdc7ece
Piotr Buda <pibuda@gmail.com>
2012-09-11 15:49:01 +0200

    Moved Twitter api creation to separate method
    Preparing to introduce DI so that Twitter implementation is injected (needed for unit testing of callback method)

--- src/main/java/eu/piotrbuda/twittory/signin/TwitterSignInResource.java
+++ src/main/java/eu/piotrbuda/twittory/signin/TwitterSignInResource.java
@@ -24,14 +24,12 @@ public class TwitterSignInResource {
     @GET
     @Path("signin")
     public Response signIn(@Context HttpServletRequest request) {
-        Twitter twitter = new TwitterFactory().getInstance();
-        twitter.setOAuthConsumer(System.getenv("twitter4j.oauth.consumerKey"), System.getenv("twitter4j.oauth.consumerSecret"));
         String requestUrl = request.getRequestURL().toString();
         String requestUri = request.getRequestURI();
         String server = requestUrl.substring(0, requestUrl.indexOf(requestUri));
         String callback = server + "/api/twitter/callback";
         try {
-            RequestToken requestToken = twitter.getOAuthRequestToken(callback);
+            RequestToken requestToken = getTwitter().getOAuthRequestToken(callback);
             request.getSession(true).setAttribute("requestToken", requestToken);
             return Response.status(302).location(URI.create(requestToken.getAuthenticationURL())).build();
         } catch (TwitterException e) {
@@ -39,16 +37,20 @@ public class TwitterSignInResource {
         }
     }
 
+    private Twitter getTwitter() {
+        Twitter twitter = new TwitterFactory().getInstance();
+        twitter.setOAuthConsumer(System.getenv("twitter4j.oauth.consumerKey"), System.getenv("twitter4j.oauth.consumerSecret"));
+        return twitter;
+    }
+
     @GET
     @Path("callback")
     public Response callback(@Context HttpServletRequest request,
                              @QueryParam("oauth_token") String oauth_token,
                              @QueryParam("oauth_verifier") String oauth_verifier) {
-        Twitter twitter = new TwitterFactory().getInstance();
-        twitter.setOAuthConsumer(System.getenv("twitter4j.oauth.consumerKey"), System.getenv("twitter4j.oauth.consumerSecret"));
         RequestToken requestToken = (RequestToken) request.getSession().getAttribute("requestToken");
         try {
-            AccessToken accessToken = twitter.getOAuthAccessToken(requestToken, oauth_verifier);
+            AccessToken accessToken = getTwitter().getOAuthAccessToken(requestToken, oauth_verifier);
             storeAccessToken(accessToken);
             return Response.seeOther(URI.create("../twittory.html")).build();
         } catch (TwitterException e) {
