0a084d6c77aa2c6607bd64f1877e6072d4601df4
Piotr Buda <pibuda@gmail.com>
2012-09-17 00:20:43 +0200

    Added method for obtaining details of access token based on access token key from external source. This is stubbed now in order for tests to proceed.

--- core/src/test/java/eu/piotrbuda/twittory/signin/when_signing_in_with_known_access_token_in_cookie.java
+++ core/src/test/java/eu/piotrbuda/twittory/signin/when_signing_in_with_known_access_token_in_cookie.java
@@ -1,17 +1,22 @@
 package eu.piotrbuda.twittory.signin;
 
+import eu.piotrbuda.twittory.storage.AccessTokenStorage;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import twitter4j.auth.AccessToken;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-import static junit.framework.Assert.*;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -25,6 +30,9 @@ public class when_signing_in_with_known_access_token_in_cookie {
     @Mock
     private HttpServletRequest request;
 
+    @Mock
+    private AccessTokenStorage storage;
+
     @Test
     public void then_user_is_redirected_to_twittory_main_page() {
         givenACookieWithAccessToken();
@@ -37,13 +45,18 @@ public class when_signing_in_with_known_access_token_in_cookie {
     }
 
     private void andKnownAccessTokenSecret() {
-        fail("Accessing access tokens is not possible at the moment");
+        when(storage.getAccessTokenDetails(anyString())).thenReturn(new AccessToken("1-a", "secret"));
     }
 
     private void userIsRedirectedToTwittory() {
         Response response = resource.signIn(request);
+        accessTokenDetailsWereRetrieved();
         assertThatResponse(response).isRedirect();
         assertThatResponse(response).redirectsTo("twittory.html");
+    }
+
+    private void accessTokenDetailsWereRetrieved() {
+        verify(storage).getAccessTokenDetails(anyString());
     }
 
     private ResponseAssertion assertThatResponse(Response response) {
--- core/src/test/java/eu/piotrbuda/twittory/signin/when_signing_in_with_unknown_access_token_in_cookie.java
+++ core/src/test/java/eu/piotrbuda/twittory/signin/when_signing_in_with_unknown_access_token_in_cookie.java
@@ -1,5 +1,6 @@
 package eu.piotrbuda.twittory.signin;
 
+import eu.piotrbuda.twittory.storage.AccessTokenStorage;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -11,8 +12,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-import static junit.framework.Assert.*;
-import static org.mockito.Mockito.mock;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -24,7 +27,10 @@ public class when_signing_in_with_unknown_access_token_in_cookie {
     private TwitterSignInResource resource;
 
     @Mock
-    private HttpServletRequest request = mock(HttpServletRequest.class);
+    private HttpServletRequest request;
+
+    @Mock
+    private AccessTokenStorage storage;
 
     @Test
     public void then_user_is_redirected_to_twitter_for_authentication() {
@@ -38,13 +44,18 @@ public class when_signing_in_with_unknown_access_token_in_cookie {
     }
 
     private void andUnknownAccessTokenSecret() {
-        fail("Accessing access tokens is not possible at the moment");
+        when(storage.getAccessTokenDetails(anyString())).thenReturn(null);
     }
 
     private void userIsRedirectedToTwitterForAuthentication() {
         Response response = resource.signIn(request);
+        accessTokenDetailsWereRetrieved();
         assertThatResponse(response).isRedirect();
         assertThatResponse(response).redirectsTo("http://api.twitter.com/oauth/authenticate?oauth_token=");
+    }
+
+    private void accessTokenDetailsWereRetrieved() {
+        verify(storage).getAccessTokenDetails(anyString());
     }
 
     private ResponseAssertion assertThatResponse(Response response) {
