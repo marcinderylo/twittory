04a136818f167d38bcb49d629a45bbc4f7e7ee3c
Piotr Buda <pibuda@gmail.com>
2012-09-17 19:54:21 +0200

    Added cookie assertion to ResponseAssertion

--- core/src/test/java/eu/piotrbuda/utils/ResponseAssertion.java
+++ core/src/test/java/eu/piotrbuda/utils/ResponseAssertion.java
@@ -1,5 +1,6 @@
 package eu.piotrbuda.utils;
 
+import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
@@ -28,5 +29,13 @@ public class ResponseAssertion {
         assertTrue(response.getMetadata().containsKey("Location"));
         List<Object> location = response.getMetadata().get("Location");
         assertTrue(location.get(0).toString().contains(url));
+    }
+
+    public void containsCookie(String cookieName, String cookieValue) {
+        assertTrue(response.getMetadata().containsKey("Set-Cookie"));
+        List<Object> cookies = response.getMetadata().get("Set-Cookie");
+        NewCookie cookie = (NewCookie) cookies.get(0);
+        assertEquals(cookieName, cookie.getName());
+        assertEquals(cookieValue, cookie.getValue());
     }
 }
