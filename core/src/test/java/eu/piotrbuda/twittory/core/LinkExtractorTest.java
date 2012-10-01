b8ac381c56da1911f32bb389118f37f07a4e6979
Marcin Deryło <marcinderylo@gmail.com>
2012-10-01 18:45:08 +0200

    merging unit tests refactoring from my fork of the original twittory repo

--- core/src/test/java/eu/piotrbuda/twittory/core/LinkExtractorTest.java
+++ core/src/test/java/eu/piotrbuda/twittory/core/LinkExtractorTest.java
@@ -1,56 +1,69 @@
 package eu.piotrbuda.twittory.core;
 
-import org.junit.Before;
 import org.junit.Test;
 import twitter4j.Status;
 
+import static java.lang.String.format;
 import static junit.framework.Assert.assertEquals;
-import static org.mockito.BDDMockito.given;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-/**
- * .
- */
 public class LinkExtractorTest {
-    private LinkExtractor extractor;
-    private Status status;
-
-    @Before
-    public void setUp() throws Exception {
-        extractor = new LinkExtractor();
-        status = mock(Status.class);
-    }
+    private LinkExtractor extractor = new LinkExtractor(); // that's JUnit - it behaves like a good citizen so can do it
 
     @Test(expected = NullPointerException.class)
     public void throws_exception_when_status_is_null() throws Exception {
-        extractor.extractLinkFrom(null);
+        extractor.extractLinkFrom(null);// oh come on, do we really need to be THAT defensive? who'd pass a null in there?
+        // I guess twitter timeline doesn't have null tweets...
     }
 
     @Test
     public void returns_empty_string_for_status_without_links() throws Exception {
-        given(status.getText()).willReturn("There is no link in status");
-        String link = extractor.extractLinkFrom(status);
-        assertEquals("", link);
+        assertThatTweet("There is no link in status").
+                hasNoLinks();
     }
 
     @Test
     public void returns_link_that_begins_with_http() throws Exception {
-        given(status.getText()).willReturn("There a link in status: http://www.onet.pl");
-        String link = extractor.extractLinkFrom(status);
-        assertEquals("http://www.onet.pl", link);
+        assertThatTweet("There a link in status: http://www.onet.pl").
+                hasLink("http://www.onet.pl");
     }
 
     @Test
     public void returns_link_that_begins_with_http_with_text_after_link() throws Exception {
-        given(status.getText()).willReturn("There a link in status: http://www.onet.pl in the middle of the text");
-        String link = extractor.extractLinkFrom(status);
-        assertEquals("http://www.onet.pl", link);
+        assertThatTweet("There a link in status: http://www.onet.pl in the middle of the text").
+                hasLink("http://www.onet.pl");
     }
 
     @Test
     public void returns_link_that_begins_with_www() throws Exception {
-        given(status.getText()).willReturn("There a link in status: www.onet.pl");
-        String link = extractor.extractLinkFrom(status);
-        assertEquals("http://www.onet.pl", link);
+        assertThatTweet("There a link in status: www.onet.pl").
+                hasLink("http://www.onet.pl");
+    }
+
+
+    private LinkExtractorAssertion assertThatTweet(String text) {
+        Status tweet = mock(Status.class);
+        when(tweet.getText()).thenReturn(text);
+        return new LinkExtractorAssertion(tweet);
+    }
+
+    private class LinkExtractorAssertion {
+        private Status tweet;
+
+        public LinkExtractorAssertion(Status tweet) {
+            this.tweet = tweet;
+        }
+
+        public void hasLink(String expectedLink) {
+            String foundLink = extractor.extractLinkFrom(tweet);
+            assertEquals(format("expected link in tweet: \"%s\"", tweet.getText()), expectedLink, foundLink);
+        }
+
+        public void hasNoLinks() {
+            String foundLink = extractor.extractLinkFrom(tweet);
+            assertTrue(format("expected no links in tweet: \"%s\"", tweet.getText()), "".equals(foundLink));
+        }
     }
 }
--- core/src/test/java/eu/piotrbuda/twittory/core/TweetLinkScannerTest.java
+++ core/src/test/java/eu/piotrbuda/twittory/core/TweetLinkScannerTest.java
@@ -1,69 +1,103 @@
 package eu.piotrbuda.twittory.core;
 
 import eu.piotrbuda.twittory.core.model.LinkDetails;
-import org.junit.After;
-import org.junit.Before;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.internal.matchers.TypeSafeMatcher;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-/**
- * .
- */
+@RunWith(MockitoJUnitRunner.class)
 public class TweetLinkScannerTest {
-    private TweetLinkScanner scanner;
-
+    @Mock
     private Twitter twitter;
-
-    @Before
-    public void setUp() throws Exception {
-        twitter = mock(Twitter.class);
-        scanner = new TweetLinkScanner(twitter);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        scanner = null;
-    }
+    @InjectMocks
+    private TweetLinkScanner scanner;
 
     @Test
     public void returns_initialized_empty_list_no_links() throws Exception {
-        ResponseList<Status> mockResponse = mock(ResponseList.class);
-        when(mockResponse.iterator()).thenReturn(Collections.EMPTY_LIST.iterator());
-        when(twitter.getHomeTimeline()).thenReturn(mockResponse);
-        List<LinkDetails> scan = scanner.scan();
-        assertNotNull(scan);
-        assertEquals(0, scan.size());
+        givenTimelineContains(noTweets());
+        thenScanForLinksShouldProduce(emptyList());
     }
 
     @Test
     public void returns_details_only_for_tweets_with_links() throws Exception {
-        Status status1 = prepareStatus("Status 1");
-        Status status2 = prepareStatus("Text with link http://www.onet.pl in it");
-        Status status3 = prepareStatus("Status 3");
-        ResponseList<Status> mockResponse = mock(ResponseList.class);
-
-        when(mockResponse.iterator()).thenReturn(Arrays.asList(status1, status2, status3).iterator());
-        when(twitter.getHomeTimeline()).thenReturn(mockResponse);
-
-        List<LinkDetails> scan = scanner.scan();
-        assertNotNull(scan);
-        assertEquals(1, scan.size());
+        givenTimelineContains(aTweetWithNoLinks(), tweet("Text with link http://www.onet.pl in it"), aTweetWithNoLinks());
+        thenScanForLinksShouldProduce(nResults(1));
     }
 
-    private Status prepareStatus(String text) {
+    @Test
+    @Ignore("ooops, this one is not yet implemented!")
+    public void returns_multiple_links_from_single_tweet() throws Exception {
+        givenTimelineContains(tweet("this tweet has two links: http://www.onet.pl and http://www.redtube.com ;)"));
+        thenScanForLinksShouldProduce(nResults(2));
+    }
+
+    private void givenTimelineContains(Status... tweets) throws TwitterException {
+        ResponseList<Status> response = mock(ResponseList.class);
+        when(response.iterator()).thenReturn(asList(tweets).iterator());
+        when(twitter.getHomeTimeline()).thenReturn(response);
+    }
+
+    private void thenScanForLinksShouldProduce(List<LinkDetails> expectedList) {
+        List<LinkDetails> scan = scanner.scan();
+        assertEquals(expectedList, scan);
+    }
+
+    private void thenScanForLinksShouldProduce(Matcher<List<LinkDetails>> expectedListMatcher) {
+        List<LinkDetails> scan = scanner.scan();
+        assertThat(scan, expectedListMatcher);
+    }
+
+    private Status aTweetWithNoLinks() {
+        return tweet("Status " + Math.random());
+    }
+
+    private Status[] noTweets() {
+        return new Status[0];
+    }
+
+
+    private Status tweet(String text) {
         Status status = mock(Status.class);
         when(status.getText()).thenReturn(text);
         return status;
+    }
+
+    private List<LinkDetails> emptyList() {
+        return Collections.emptyList();
+    }
+
+    private Matcher<List<LinkDetails>> nResults(final int expectedNumberOfResults) {
+        return new TypeSafeMatcher<List<LinkDetails>>() {
+            @Override
+            public boolean matchesSafely(List<LinkDetails> item) {
+                return item.size() == expectedNumberOfResults;
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("list with ");
+                description.appendValue(expectedNumberOfResults);
+                description.appendText(" elements");
+            }
+        };
     }
 }
